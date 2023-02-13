59731f486c12f9dcd1b729a668baa00f8d8845a8
taeck <taeck007@gmail.com>
2023-02-13 19:17:59 +0900

    2주차 과제 제출

---
+++ src/App.ts
@@ -0,0 +1,30 @@
+import React, { useCallback, useState } from 'react';
+
+import Greeting from './components/Greeting';
+
+type ImageProps = {
+  src: string;
+  alt?: string;
+  width?: number;
+}
+
+function Image({ src, alt = '', width }: ImageProps) {
+  return React.createElement('img', { src, alt, width: width ?? 'auto' });
+}
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = (value: number) => {
+    setCount(count + value);
+  };
+
+  return (
+    React.createElement('div', {},
+      React.createElement(Greeting, { name: 'wholeman' }),
+      React.createElement(Image, { src: "/images/test.jpg", alt: "Test Image", width: 200 }),
+      React.createElement('p', {}, 'Count:', ' ', count),
+      ...[1, 2, 3, 4, 5].map((i) => React.createElement('button', { key: i, type: "button", onClick: () => handleClick(i) }, `+${i}`))
+    )
+  );
+}

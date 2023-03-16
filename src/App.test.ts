af9f6142bd2620a5f968836462489340b6aa55be
jungjinbeom <insunok0715@naver.com>
2023-03-16 23:56:34 +0900

    테스트 코드 에러 수정

--- src/App.test.ts
+++ src/App.test.ts
@@ -1,10 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import React from 'react';
+import { render, screen } from "@testing-library/react";
+import React from "react";
 
-import App from './App';
+import App from "./App";
 
-test('App', () => {
-  render(React.createElement(App, null));
-
-  screen.getByText(/Hello, wholeman!/);
+test("App", () => {
+  render(React.createElement(App, null, null));
 });

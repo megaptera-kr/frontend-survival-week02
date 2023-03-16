388f1a513db77d65c6d0c026cc481a080c4ce8e8
jungjinbeom <insunok0715@naver.com>
2023-03-16 23:45:32 +0900

    2주차 과제 JSX없이 React 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import { useState } from 'react';
-
-import Greeting from './components/Greeting';
+import React, { useState } from "react";
+import Greeting from "./components/Greeting";
 
 type ImageProps = {
   src: string;
   alt?: string;
   width?: number;
-}
+};
 
-function Image({ src, alt = '', width }: ImageProps) {
-  return (
-    <img src={src} alt={alt} width={width ?? 'auto'} />
-  );
+function Image({ src, alt = "", width }: ImageProps) {
+  return React.createElement("img", { src, alt, width: width ?? "auto" });
 }
 
 export default function App() {
@@ -21,21 +18,25 @@ export default function App() {
     setCount(count + value);
   };
 
-  return (
-    <div>
-      <Greeting name="wholeman" />
-      <Image src="/images/test.jpg" alt="Test Image" width={200} />
-      <p>
-        Count:
-        {' '}
-        {count}
-      </p>
-      {[1, 2, 3, 4, 5].map((i) => (
-        <button key={i} type="button" onClick={() => handleClick(i)}>
-          +
-          {i}
-        </button>
-      ))}
-    </div>
+  return React.createElement(
+    "div",
+    null,
+    React.createElement(
+      Greeting,
+      { name: "wholeman" },
+      React.createElement(Image, {
+        src: "/images/test.jpg",
+        alt: "Test Image",
+        width: 200,
+      }),
+      React.createElement("p", { name: "wholeman" }),
+      [1, 2, 3, 4, 5].map((i) =>
+        React.createElement(
+          "button",
+          { key: i, type: "button", onClick: () => handleClick(i) },
+          +i
+        )
+      )
+    )
   );
 }

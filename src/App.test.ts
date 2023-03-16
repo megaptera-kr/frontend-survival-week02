30dce7d35cd8da9817b1e697569d9339a8bd8fe5
jungjinbeom <insunok0715@naver.com>
2023-03-16 23:58:41 +0900

    코드 컨벤션 에러 수정

--- src/App.test.ts
+++ src/App.test.ts
@@ -1,8 +1,8 @@
-import { render, screen } from "@testing-library/react";
-import React from "react";
+import { render, screen } from '@testing-library/react';
+import React from 'react';
 
-import App from "./App";
+import App from './App';
 
-test("App", () => {
+test('App', () => {
   render(React.createElement(App, null, null));
 });

60b9ca00682a3b0fe16cadd2043fd31ff6053d31
haeun kim <ckhe1215@gmail.com>
2023-03-18 19:26:50 +0900

    fix JSX to React.createElement

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 import Greeting from './components/Greeting';
 
@@ -10,7 +10,7 @@ type ImageProps = {
 
 function Image({ src, alt = '', width }: ImageProps) {
   return (
-    <img src={src} alt={alt} width={width ?? 'auto'} />
+    React.createElement('img', { src, alt, width: width ?? 'auto' })
   );
 }
 
@@ -22,20 +22,19 @@ export default function App() {
   };
 
   return (
-    <div>
-      <Greeting name="wholeman" />
-      <Image src="/images/test.jpg" alt="Test Image" width={200} />
-      <p>
-        Count:
-        {' '}
-        {count}
-      </p>
-      {[1, 2, 3, 4, 5].map((i) => (
-        <button key={i} type="button" onClick={() => handleClick(i)}>
-          +
-          {i}
-        </button>
-      ))}
-    </div>
+    React.createElement(
+      'div',
+      null,
+      React.createElement(Greeting, { name: 'wholeman' }),
+      React.createElement(Image, { src: '/images/test.jpg', alt: 'Test Image', width: 200 }),
+      React.createElement('p', null, 'Count:', ' ', count),
+      [1, 2, 3, 4, 5].map((i) => (React.createElement('button', {
+        key: i,
+        type: 'button',
+        onClick() {
+          handleClick(i);
+        },
+      }, '+', i))),
+    )
   );
 }

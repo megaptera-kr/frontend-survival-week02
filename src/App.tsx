e78aa55e84c9f209022e61fb6d77510089edf25f
ShinjungOh <tlswjd9260@gmail.com>
2023-03-19 11:31:36 +0900

    feat: JavaScript 코드에 React.createElement 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 import Greeting from './components/Greeting';
 
@@ -8,10 +8,16 @@ type ImageProps = {
   width?: number;
 }
 
-function Image({ src, alt = '', width }: ImageProps) {
-  return (
-    <img src={src} alt={alt} width={width ?? 'auto'} />
-  );
+function Image({
+  src,
+  alt = '',
+  width,
+}: ImageProps) {
+  return React.createElement('img', {
+    src,
+    alt,
+    width: width || 'auto',
+  });
 }
 
 export default function App() {
@@ -22,20 +28,21 @@ export default function App() {
   };
 
   return (
-    <div>
-      <Greeting name="wholeman" />
-      <Image src="/images/test.jpg" alt="Test Image" width={200} />
-      <p>
-        Count:
-        {' '}
-        {count}
-      </p>
-      {[1, 2, 3, 4, 5].map((i) => (
-        <button key={i} type="button" onClick={() => handleClick(i)}>
-          +
-          {i}
-        </button>
-      ))}
-    </div>
+    React.createElement(
+      'div',
+      null,
+      React.createElement(Greeting, { name: 'wholeman' }),
+      React.createElement(Image, {
+        src: '/images/test.jpg',
+        alt: 'Test Image',
+        width: 200,
+      }),
+      React.createElement('p', null, 'Count:', ' ', count),
+      [1, 2, 3, 4, 5].map((i) => React.createElement('button', {
+        key: i,
+        type: 'button',
+        onClick: () => handleClick(i),
+      }, '+', i)),
+    )
   );
 }

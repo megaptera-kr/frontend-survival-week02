a4bcba799aaa195c5d5176a2dda7789f95f6cf03
lswon2885 <lswon2885@daum.net>
2023-04-21 17:26:30 +0900

    2주차 과제

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-function main() {
+function main(): void {
   const container = document.getElementById('root');
   if (!container) {
     return;
   }
-
   const root = ReactDOM.createRoot(container);
-  root.render(<App />);
+  root.render(React.createElement(App, null));
 }
-
 main();

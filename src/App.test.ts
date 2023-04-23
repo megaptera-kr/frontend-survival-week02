ca797633d1eb7975c850a03545a45e051f61db4d
dico1631 <dicoqnfkrzl@gmail.com>
2023-04-23 12:16:37 +0900

    2주차 과제

--- src/App.test.ts
+++ src/App.test.ts
@@ -1,9 +1,10 @@
+import React from react;
 import { render, screen } from '@testing-library/react';
 
 import App from './App';
 
 test('App', () => {
-  render(<App />);
+  render(React.createElement(App, null, null));
 
   screen.getByText(/Hello, wholeman!/);
 });
---
+++ src/components/Greeting.ts
@@ -0,0 +1,6 @@
+import React from react;
+export default function Greeting({ name }: {
+  name: string;
+}) {
+  return React.createElement('p', { name }, 'Hello, ', name, '!');
+}

efef411b5454a3fe36c10c7ce74e2f1d63bff1fa
김민주 <canon.minjoo@gmail.com>
2023-07-27 20:52:59 +0900

    git push test

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -1,7 +1,7 @@
 export default function Greeting({ name }: { name: string }) {
   return (
     <p>
-      Hello, Hello, Hello,
+      Hello, Hello, Hello, Hello, Hello, Hello,Hello, Hello, Hello,
       {name}!
     </p>
   );

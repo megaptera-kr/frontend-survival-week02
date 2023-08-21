7fc24fbaa13cf1dd416deb31a74ce84abcd49582
u-ryu-00 <lwh76104@gmail.com>
2023-08-21 21:43:01 +0900

    [류유정] 2주차 과제 - JSX 없이 React 만들기

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
 import { render, screen } from '@testing-library/react';
 
+import React from 'react';
+
 import App from './App';
 
 test('App', () => {
-  render(<App />);
+  render(React.createElement(App, null));
 
   screen.getByText(/Hello, wholeman!/);
 });

93c0497845fa84c9df761054989d82233c98da96
shcha <>
2023-09-24 19:58:02 +0900

    git push origin origin ChaShDv -m '2주차 과제 v1.0.0' i

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState } from 'react';
 
 import Greeting from './components/Greeting';
@@ -10,7 +11,8 @@ type ImageProps = {
 
 function Image({ src, alt = '', width }: ImageProps) {
   return (
-    <img src={src} alt={alt} width={width ?? 'auto'} />
+    //<img src={src} alt={alt} width={width ?? 'auto'} />
+    React.createElement('img', {src, alt, width: width || 'auto'})
   );
 }
 
@@ -21,21 +23,35 @@ export default function App() {
     setCount(count + value);
   };
 
-  return (
-    <div>
-      <Greeting name="wholeman" />
-      <Image src="/images/test.jpg" alt="Test Image" width={200} />
-      <p>
-        Count:
-        {' '}
-        {count}
-      </p>
-      {[1, 2, 3, 4, 5].map((i) => (
-        <button key={i} type="button" onClick={() => handleClick(i)}>
-          +
-          {i}
-        </button>
-      ))}
-    </div>
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(Greeting, {name: 'wholeman'}),
+    React.createElement(Image, {
+      src: '/images/test.jpg',
+      alt: 'Test Image',
+      width: 200,
+    }),
+    /*
+    <p>
+      Count:
+      {' '}
+      {count}
+    </p>
+    {[1, 2, 3, 4, 5].map((i) => (
+      <button key={i} type="button" onClick={() => handleClick(i)}>
+        +
+        {i}
+      </button>
+    ))}
+    */
+    React.createElement('p', null, 'Count:', ' ', count)
+    ,[1,2,3,4,5].map((i) => React.createElement(
+      'button',
+      {key: i, type: 'button', onClick: () => handleClick},
+      // 여기부터 태그 안에 들어가는 값
+      '+', 
+      i
+    ))
   );
 }

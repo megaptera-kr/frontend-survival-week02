414d1be29aed286cc6c7b20f9a916da92688fd4a
uhuikim <uhui94@naver.com>
2023-10-24 23:08:09 +0900

    mod : jsx사용하지 않기!React.createElement로 변경

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -1,12 +1,7 @@
+import React from 'react';
+
 export default function Greeting({ name }: {
   name: string;
 }) {
-  return (
-    <p>
-      Hello,
-      {' '}
-      {name}
-      !
-    </p>
-  );
+  return React.createElement('p', null, 'Hello,', ' ', name, '!');
 }

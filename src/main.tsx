80aefbf118f9f022a9172f5f60d5176819162ee1
yunamoon <tpwkao0051@gmail.com>
2023-12-25 17:54:59 +0900

    yunamoon_2week

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import App from './App';
 
 function main() {
@@ -9,7 +9,8 @@ function main() {
   }
 
   const root = ReactDOM.createRoot(container);
-  root.render(<App />);
+  const element = React.createElement(App, null, null);
+  root.render(element);
 }
 
 main();

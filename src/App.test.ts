db0f3241649a973ab13eaf2f0161adfc9899ae4e
doyunnn <73818291+doyunnn@users.noreply.github.com>
2024-02-04 18:34:33 +0900

    [Refactor] Create App without JSX

--- src/App.test.ts
+++ src/App.test.ts
@@ -1,9 +1,11 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 
 import App from './App';
 
 test('App', () => {
-  render(<App />);
+  const appComponent = React.createElement(App, null);
+  render(appComponent);
 
   screen.getByText(/Hello, wholeman!/);
 });

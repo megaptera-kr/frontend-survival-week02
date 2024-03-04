76a8ed342c3ea84acf9c960fdb7fb1ba8facfb70
BonHyun Koo <bonhyunkoo@BonHyunui-MacBookPro.local>
2024-03-04 19:29:18 +0900

    [11기 구본현] 2주차 과제 JSX 없이 React 만들기

---
+++ src/App.ts
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+import Greeting from './components/Greeting';
+
+type ImageProps = {
+  src: string;
+  alt?: string;
+  width?: number;
+};
+
+function Image({ src, alt = '', width }: ImageProps) {
+  return React.createElement('img', { src, alt, width: width ?? 'auto' });
+}
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = (value: number) => {
+    setCount(count + value);
+  };
+
+  const element = React.createElement;
+
+  return element(
+    'div',
+    null,
+    element(Greeting, { name: 'wholeman' }),
+    element(Image, {
+      src: '/images/test.jpg',
+      alt: 'Test Image',
+      width: 200,
+    }),
+    element('p', null, 'Count: ', count),
+    [1, 2, 3, 4, 5].map((i) =>
+      element(
+        'button',
+        { key: i, type: 'button', onClick: () => handleClick(i) },
+        `+${i}`
+      )
+    )
+  );
+}
+
+// <div>
+// <Greeting name='wholeman' />
+// <Image src='/images/test.jpg' alt='Test Image' width={200} />
+// <p>Count: {count}</p>
+// {[1, 2, 3, 4, 5].map((i) => (
+//   <button key={i} type='button' onClick={() => handleClick(i)}>
+//     +{i}
+//   </button>
+// ))}
+// </div>

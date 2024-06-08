bc4d0f67e329152872837e5a70e4fccb0991bb43
ain <lovepizza132@gmail.com>
2024-06-08 18:26:15 +0900

    refactor: jsx 사용제거

--- src/App.test.ts
+++ src/App.test.ts
@@ -1,9 +1,13 @@
 import { render, screen } from '@testing-library/react';
 
+import React from 'react';
+
 import App from './App';
 
 test('App', () => {
-  render(<App />);
+  const element = React.createElement(App, null, null);
+
+  render(element);
 
   screen.getByText(/Hello, wholeman!/);
 });
